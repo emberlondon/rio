c3e6733cd03b9ac539b05676a045ab1f598367fb
Jamie White <jamie@jgwhite.co.uk>
2016-08-18 22:18:48 +0100

    Add animation

---
+++ app/components/team-card.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+import computed from 'ember-computed';
+import { next } from 'ember-runloop';
+import { barLength } from 'rio/helpers/bar-length';
+import InViewportMixin from 'ember-in-viewport';
+
+export default Ember.Component.extend(InViewportMixin, {
+  counts: computed('open', 'team.{gold_count,silver_count,bronze_count}', function() {
+    if (this.get('open')) {
+      let {
+        gold_count,
+        silver_count,
+        bronze_count
+      } = this.get('team');
+
+      return {
+        gold: gold_count,
+        silver: silver_count,
+        bronze: bronze_count
+      };
+    } else {
+      return {
+        gold: 0,
+        silver: 0,
+        bronze: 0
+      };
+    }
+  }),
+
+  didEnterViewport() {
+    next(() => this.set('open', true));
+  }
+});
